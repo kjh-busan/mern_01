1a9041d2103d2835be2b2fcdaba31c6906f76dec
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-04-14 21:43:42 +0900

    feat: create done

--- backend/models/User.js
+++ backend/models/User.js
@@ -9,13 +9,13 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true,
   },
-  // 다른 필드들...
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,7 +2,28 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User"); // User 모델 경로에 맞게 조정
 
-// '/api/users' 경로로 GET 요청이 오면 실행될 핸들러
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body; // 요청 바디에서 필드 추출
+  console.log("#1 [CREATE]:", name, email, password);
+
+  try {
+    // 새로운 사용자 문서 생성
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    // MongoDB에 사용자 삽입
+    const result = await newUser.save();
+
+    res.status(201).json(result); // 삽입된 사용자 정보를 클라이언트에게 응답
+  } catch (error) {
+    console.error("Error inserting user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 router.get("/", async (req, res) => {
   const { name } = req.query; // 쿼리 파라미터에서 'name' 추출
   try {
@@ -14,7 +35,7 @@ router.get("/", async (req, res) => {
     }
 
     const users = await User.find(query); // 수정된 쿼리 객체를 사용하여 사용자 조회
-    console.log("#1 검색name:", name); // 콘솔창에 출력
+    console.log("#2 [READ]:", name); // 콘솔창에 출력
     res.json(users); // 클라이언트에게도 결과 전송
   } catch (err) {
     console.error(err.message);
@@ -22,6 +43,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// TODO: UPDATE
 router.delete("/:id", async (req, res) => {
   const userId = req.params.id; // 라우트 경로에서 매개변수를 읽어옵니다.
   console.log("#4 [DELETE] userId:", userId);
