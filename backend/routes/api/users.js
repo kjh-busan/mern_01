96c4af87337eca347f7de4f153d1980b2d2140c9
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-04-14 23:57:01 +0900

    feat: update done [BE]

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -26,6 +26,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   const { name } = req.query; // 쿼리 파라미터에서 'name' 추출
+  console.log("#2 [READ]:", name); // 콘솔창에 출력
   try {
     let query = {};
 
@@ -35,7 +36,6 @@ router.get("/", async (req, res) => {
     }
 
     const users = await User.find(query); // 수정된 쿼리 객체를 사용하여 사용자 조회
-    console.log("#2 [READ]:", name); // 콘솔창에 출력
     res.json(users); // 클라이언트에게도 결과 전송
   } catch (err) {
     console.error(err.message);
@@ -43,7 +43,39 @@ router.get("/", async (req, res) => {
   }
 });
 
-// TODO: UPDATE
+router.put("/:id", async (req, res) => {
+  const userId = req.params.id; // 라우트 경로에서 사용자 ID를 읽어옵니다.
+  const { name, email, password } = req.body; // 요청 본문에서 필드를 추출합니다.
+  console.log("#3 [UPDATE]:", name, email, password);
+  try {
+    // MongoDB에서 해당 ID를 가진 사용자를 찾습니다.
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // 클라이언트에서 전달된 필드가 있는 경우에만 업데이트합니다.
+    if (name) {
+      user.name = name;
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+
+    // 업데이트된 사용자 정보를 저장합니다.
+    const updatedUser = await user.save();
+
+    res.status(200).json(updatedUser); // 업데이트된 사용자 정보를 클라이언트에게 응답합니다.
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   const userId = req.params.id; // 라우트 경로에서 매개변수를 읽어옵니다.
   console.log("#4 [DELETE] userId:", userId);
