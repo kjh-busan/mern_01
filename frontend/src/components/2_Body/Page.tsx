97648c80566ab47e8c722eacf00cbb5c43206689
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-05-18 11:23:35 +0900

    chore: modify directory trees

--- frontend/src/components/2_Body/Page.tsx
+++ frontend/src/components/2_Body/Page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Header } from '../1_Headers/Header'
 import Footer from '../9_Footers/Footer'
-import Todo from '../3_Page/MongoDB/SampleFlix/Todo'
+import Todo from '../pages/MongoDB/Todo'
 
 type User = {
     name: string
