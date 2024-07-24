a6f51633a2010b0d15b19c4a966bb5cc5825ab82
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-07-24 22:52:16 +0900

    feat: add config for jest test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js', 'jest-given'],
     testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.(ts|tsx)$': 'babel-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    transformIgnorePatterns: ['/node_modules/'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
 }
