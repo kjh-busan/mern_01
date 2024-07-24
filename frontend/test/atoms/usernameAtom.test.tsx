0a013f3febab1bf9227d0e6f12a9dfc49da77c14
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-07-25 00:04:04 +0900

    feat: jest WIP

--- frontend/test/atoms/usernameAtom.test.tsx
+++ frontend/test/atoms/usernameAtom.test.tsx
@@ -1,30 +1,33 @@
-// test/atoms/usernameAtom.test.ts
+import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
 import { Provider, useAtom } from 'jotai'
-import { renderHook, act } from '@testing-library/react-hooks'
 import { usernameAtom } from '../../src/atoms/atoms'
+import { createRoot } from 'react-dom/client'
+
+const Wrapper: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+    const root = createRoot(container)
+    root.render(<Provider>{children}</Provider>)
+    return <>{children}</>
+}
 
 describe('usernameAtom', () => {
     it('should have initial state as null', () => {
         const { result } = renderHook(() => useAtom(usernameAtom), {
-            wrapper: Provider,
+            wrapper: Wrapper,
         })
-
         const [username] = result.current
         expect(username).toBeNull()
     })
 
     it('should update the username state', () => {
         const { result } = renderHook(() => useAtom(usernameAtom), {
-            wrapper: Provider,
+            wrapper: Wrapper,
         })
-
         const [, setUsername] = result.current
-
-        act(() => {
-            setUsername('new_user')
-        })
-
+        setUsername('testUser')
         const [username] = result.current
-        expect(username).toBe('new_user')
+        expect(username).toBe('testUser')
     })
 })
